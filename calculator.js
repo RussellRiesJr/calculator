b86d1ceb5b8b178a3265216b73f8b98d59c5e45c
Russell Ries <riesru@gmail.com>
2016-04-22 13:49:30 -0500

    add HTML elements & basic functions

--- calculator.js
+++ calculator.js
@@ -1,29 +1,41 @@
-// In your JavaScript, put an event listener on each of the buttons.
+// Place to send the answers
+var yourAnswer = document.getElementByID("outputAnswer");
 
+// Pulling the input values into JS
+var inputOne = document.getElementByID("valueOne");
+var inputTwo = document.getElementByID("valueTwo");
 
 /*
   Create a function that multiplies two numbers
   passed in as arguments. Return the product.
  */
-
+function multiply(a, b) {
+  return a * b;
+}
 
 /*
   Create a function that adds two numbers
   passed in as arguments. Return the sum.
  */
-
+function add(a, b) {
+ return a + b;
+}
 
 /*
   Create a function that subtracts two numbers
   passed in as arguments. Return the difference.
  */
-
+function subtract(a, b) {
+  return a - b;
+}
 
 /*
   Create a function that divides two numbers
   passed in as arguments. Return the quotient.
  */
-
+function divide(a, b) {
+  return a / b;
+}
 
 
 /*
@@ -34,3 +46,6 @@
 
   Return the value of the operation.
  */
+
+
+// In your JavaScript, put an event listener on each of the buttons.
