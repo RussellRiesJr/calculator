33d83e9accac881fae51e09e68c65a2af0e2242d
Russell Ries <riesru@gmail.com>
2016-04-22 14:59:37 -0500

    finish exercise

--- calculator.js
+++ calculator.js
@@ -1,42 +1,56 @@
 // Place to send the answers
-var yourAnswer = document.getElementByID("outputAnswer");
+var yourAnswer = document.getElementById("outputAnswer");
 
 // Pulling the input values into JS
-var inputOne = document.getElementByID("valueOne");
-var inputTwo = document.getElementByID("valueTwo");
+var inputOne = document.getElementById("valueOne");
+var inputTwo = document.getElementById("valueTwo");
+
+
+// Grabbing stuff
+var add = document.getElementById("addition");
+var sub = document.getElementById("subtraction");
+var mul = document.getElementById("multiplication");
+var div = document.getElementById("division");
+
+function translate(event) {
+  var newOne = parseInt(inputOne.value);
+  var newTwo = parseInt(inputTwo.value);
+  console.log(event.srcElement.id);
+  yourAnswer.innerHTML = combo(eval(event.srcElement.id), newOne, newTwo);
+}
 
 /*
   Create a function that multiplies two numbers
   passed in as arguments. Return the product.
  */
-function multiply(a, b) {
+function multiplication(a, b) {
   return a * b;
 }
-
+console.log("2 * 4=", multiplication(2,4));
 /*
   Create a function that adds two numbers
   passed in as arguments. Return the sum.
  */
-function add(a, b) {
+function addition(a, b) {
  return a + b;
 }
-
+console.log("2 + 4=", addition(2,4));
 /*
   Create a function that subtracts two numbers
   passed in as arguments. Return the difference.
  */
-function subtract(a, b) {
+function subtraction(a, b) {
   return a - b;
 }
-
+console.log("2 - 4=", subtraction(2,4));
 /*
   Create a function that divides two numbers
   passed in as arguments. Return the quotient.
  */
-function divide(a, b) {
+function division(a, b) {
   return a / b;
 }
-
+console.log("2 / 4=", division(2,4));
 
 /*
   Create a function that accepts three arguments.
@@ -46,6 +60,13 @@ function divide(a, b) {
 
   Return the value of the operation.
  */
-
+function combo(fun, a, b) {
+  console.log(fun);
+  return fun(a, b);
+}
 
 // In your JavaScript, put an event listener on each of the buttons.
+add.addEventListener("click", translate);
+sub.addEventListener("click", translate);
+mul.addEventListener("click", translate);
+div.addEventListener("click", translate);
